f2ba402c756ccf70c97d7a02aaf33171970cfa97
max <mf@conciliolabs.com>
2018-10-09 16:13:37 +0300

    stage task

--- src/main/kotlin/kengooru/KengooruApplication.kt
+++ src/main/kotlin/kengooru/KengooruApplication.kt
@@ -2,10 +2,19 @@ package kengooru
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class KengooruApplication
 
 fun main(args: Array<String>) {
     runApplication<KengooruApplication>(*args)
+}
+
+@RestController
+class PlaceController {
+    @GetMapping("/hello")
+    fun  hello() = "hello!"
+
 }

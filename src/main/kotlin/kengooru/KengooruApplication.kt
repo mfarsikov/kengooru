70325482ccabd59070277c5f2cc81870ca07777b
max <mf@conciliolabs.com>
2018-10-09 19:02:41 +0300

    create-read

--- src/main/kotlin/kengooru/KengooruApplication.kt
+++ src/main/kotlin/kengooru/KengooruApplication.kt
@@ -6,12 +6,16 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.net.URI
 import java.time.LocalDate
+import java.util.*
 import javax.persistence.CascadeType
-import javax.persistence.ElementCollection
 import javax.persistence.Entity
 import javax.persistence.FetchType
 import javax.persistence.GeneratedValue
@@ -30,24 +34,29 @@ fun main(args: Array<String>) {
 class ItineraryController(
         val itineraryRepository: ItineraryRepository
 ) {
+    @PostMapping("/itineraries")
+    fun create(@RequestBody itinerary: Itinerary):ResponseEntity<Unit>{
+        itineraryRepository.save(itinerary)
+        return ResponseEntity.created(URI("/itineraries/${itinerary.id}")).build()
+    }
+
     @GetMapping("/itineraries/{id}")
-    fun getItinerary(@PathVariable id: String): Itinerary {
-        return itineraryRepository.findAll().first()
+    fun getItinerary(@PathVariable id: UUID): Itinerary {
+        return itineraryRepository.getOne(id)
     }
 }
 
-interface ItineraryRepository : JpaRepository<Itinerary, Int>
+interface ItineraryRepository : JpaRepository<Itinerary, UUID>
 
 @Entity
 class Itinerary(
+        val ownerId: String,
         val title: String,
         val date: LocalDate,
-        @OneToMany(fetch = FetchType.EAGER , cascade = [CascadeType.ALL])
+        @OneToMany(fetch = FetchType.EAGER, cascade = [CascadeType.ALL])
         val points: List<Point>
 ) {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Int? = null
+    @Id val id = UUID.randomUUID()
 }
 
 @Entity
@@ -55,10 +64,8 @@ class Point(
         val text: String,
         val lat: Double,
         val lon: Double
-){
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Int? = null
+) {
+    @Id val id = UUID.randomUUID()
 }
 
 @Configuration
@@ -67,6 +74,7 @@ class Config {
     fun init(itineraryRepository: ItineraryRepository): ApplicationRunner {
         return ApplicationRunner {
             itineraryRepository.save(Itinerary(title = "Some title",
+                                               ownerId = "123",
                                                date = LocalDate.now(),
                                                points = listOf(Point(text = "some text", lat = 0.0, lon = 0.0))))
         }
